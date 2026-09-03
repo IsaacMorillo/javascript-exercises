5abc3ad7a1f011fd4194a61f41254e4f7eb9aca4
IsaacMorillo <irmorillowork@gmail.com>
2026-09-03 10:52:11 -0500

    Complete JavaScript fundamentals exercise 6
    Implement string repetition logic using specified iteration count
    to practice string concatenation and manipulation techniques.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, numRepeat) {
+  let newString = "";
+  if (numRepeat < 0) {
+    return "ERROR";
+  }
+  for (let i = 1; i <= numRepeat; i++) {
+    newString += string;
+  }
+  return newString;
 };
 
 // Do not edit below this line
