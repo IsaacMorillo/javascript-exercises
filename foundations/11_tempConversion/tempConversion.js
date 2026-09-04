72c9ad32eb6b3d997dd2bb43aaa76c4f983e4aae
IsaacMorillo <irmorillowork@gmail.com>
2026-09-03 19:20:03 -0500

    Complete JavaScript fundamentals exercise 11
    Implement temperature conversion functions between Celsius and
    Fahrenheit to practice numeric operations and precision methods.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureFahrenheit) {
+  let temp = (temperatureFahrenheit - 32) /1.8
+  return Number((temp).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureCelsius) {
+   let temp = (temperatureCelsius * 1.8)+ 32;
+   return Number((temp).toFixed(1));
 };
 
 // Do not edit below this line
