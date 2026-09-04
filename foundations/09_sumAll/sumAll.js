e182c521b22c1d77dd8e2f658696e3f034736f6b
IsaacMorillo <irmorillowork@gmail.com>
2026-09-03 19:07:49 -0500

    Complete JavaScript fundamentals exercise 8
    Implement range summation logic to practice loop iterations and
    control flow techniques.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (numStart, numEnd) {
+  if (!(Number.isInteger(numStart)) || !(Number.isInteger(numEnd))) {
+    return "ERROR"
+  } else if (numStart < 0 || numEnd < 0){
+    return "ERROR";
+  }
+  if (numStart > numEnd){
+    let temp = numStart;
+    numStart = numEnd;
+    numEnd = temp;
+  }
+  let acum = 0;
+  for (let i = numStart; i <= numEnd; i++) {
+    acum += i;
+  }
+  return acum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+console.log(sumAll([1, 2, 3]));
