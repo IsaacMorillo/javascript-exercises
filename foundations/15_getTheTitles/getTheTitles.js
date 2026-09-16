b56ddedbcc94142ddb077422d5492730cd19ebde
IsaacMorillo <irmorillowork@gmail.com>
2026-09-15 21:29:50 -0500

    Solve JS fundamentals exercise 15 to extract book titles
    Implement the 'getTheTitles' function using array transformation to
    return an array of title strings from a collection of book objects.

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrObj) {
+    arrAns = [];
+    for (let i=0; i < arrObj.length ; i++){
+       let title = arrObj[i]['title'] ;
+       arrAns.push(title);
+    }
+    return arrAns;
 };
 
 // Do not edit below this line
