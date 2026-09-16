8dd877245abe28af78d4f8bb639f7687dd3799e9
IsaacMorillo <irmorillowork@gmail.com>
2026-09-15 21:24:58 -0500

    Solve JS fundamentals exercise 14 for Fibonacci sequence
    Implement Fibonacci calculation with input parsing to handle numerical
    string inputs and reject negative index values.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number.parseInt(num);
+    let numPrev= 0;
+    let numAct = 1;
+    if(num < 0){
+        return 'OOPS'
+    }
+    if(num == 1){
+        return 1;
+    }
 
+    if (num == 0){
+        return 0;
+    }
+    for (let i=1; i < num ; i++){
+        let nextNum = numPrev + numAct;
+        numPrev = numAct;
+        numAct = nextNum;
+    }
+
+    return numAct;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+console.log(fibonacci(10));
