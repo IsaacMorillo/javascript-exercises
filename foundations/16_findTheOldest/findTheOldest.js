f95126e1e748176d45855317c061d80cb29a9a15
IsaacMorillo <irmorillowork@gmail.com>
2026-09-16 19:09:44 -0500

    Solve JS fundamentals exercise 16 to find the oldest person
    Implement the 'findTheOldest' function to compare ages across a list of
    person objects, handling active years and missing death dates.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arrObj) {
+  for (let i = 0; i < arrObj.length; i++) {
+    if (!("yearOfDeath" in arrObj[i])) {
+      arrObj[i].age = 2026 - arrObj[i].yearOfBirth;
+    } else {
+      arrObj[i].age = arrObj[i].yearOfDeath - arrObj[i].yearOfBirth;
+    }
+  }
+   let namePeopleOldest = arrObj[0];
+  for (let i = 1; i < arrObj.length; i++) {
+    if (arrObj[i].age > namePeopleOldest.age) {
+      namePeopleOldest = arrObj[i];
+    }
+  }
+  return namePeopleOldest;
 };
 
 // Do not edit below this line
